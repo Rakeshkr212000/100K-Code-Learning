5147a055005612dd0224b129eba1a3dbe4faabe5
RakeshKR <Rakeshkr212000@gmail.com>
2022-05-14 20:05:30 +0530

    sum of two numbers

---
+++ C Program/sum two num.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void)
+{
+    int num1,num2,sum;
+    printf("Enter two numbers : ");
+    scanf("%d%d",&num1,&num2);
+    sum=num1+num2;
+    printf("Sum of Two numbers %d",sum);
+    return EXIT_SUCCESS;
+}
