791ccbd0a605f7706963ad45aaa1c586cd7d4d09
RakeshKR <Rakeshkr212000@gmail.com>
2022-05-14 19:56:51 +0530

    One Number

---
+++ C Program/one number.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+    int a;
+    printf("ENTER ONE NUMBER : " );
+    scanf("%d",&a);
+    printf("You Entered Number is %d ",a);
+    return EXIT_SUCCESS;
+}
